cd2c43b80b74686c154b6a8603f7dbd0d3a478f3
Szymon Rucinski <szymon.rucinski@fhnw.ch>
2022-09-28 22:20:35 +0200

    ok

---
+++ install_deps.sh
@@ -0,0 +1,7 @@
+#install rclone
+sudo -v ; curl https://rclone.org/install.sh | sudo bash
+#move services
+
+#sudo cp ./services/pcloud.service /etc/systemd/system/pcloud.service
+#sudo cp ./services/seedbox.service /etc/systemd/system/seedbox.service
+
